8f0626ee1df6441e9cd2a23352f15b3dbcef6729
mgah77 <mauricio.gah@gmail.com>
2023-07-21 19:47:45 -0400

    Update estado.py

--- models/estado.py
+++ models/estado.py
@@ -16,9 +16,9 @@ class Estado_pago(models.Model):
     pre_vencido = fields.Float(string="Cantidad por vencer")
     total = fields.Float(string="Total Deuda")
 
-    @api.model
-    def create(self,vals):
-        if vals.get('name','New')=='New':
-            vals['name']=self.env['ir.sequence'].next_by_code('pre.estado') or 'New'
-        result = super(ImportTemplate,self).create(vals)
-        return result
+    #@api.model
+    #def create(self,vals):
+    #    if vals.get('name','New')=='New':
+    #        vals['name']=self.env['ir.sequence'].next_by_code('pre.estado') or 'New'
+    #    result = super(ImportTemplate,self).create(vals)
+    #    return result
