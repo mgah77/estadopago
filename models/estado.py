9d324dcb8a39f97a0f7061a589e051f3318013d6
mgah77 <mauricio.gah@gmail.com>
2023-07-22 12:42:11 -0400

    w

--- models/estado.py
+++ models/estado.py
@@ -2,19 +2,7 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
-class Estado_pago(models.Model):
-    _name = 'estadopago'
-    _descryption = 'Estado de pago de clientes'
 
-    consulta = fields.Char(string="nro consulta")
-    fecha = fields.Date(string="fecha")
-    us_id = fields.Integer(string="Usuario")
-    credito = fields.Float(string="Credito")
-    fac_vencido = fields.Integer(string="FacturasVencidas")
-    vencido = fields.Float(string="Cantidad Vencida")
-    pre_fac_vencido = fields.Integer(string="Facturas por vencer")
-    pre_vencido = fields.Float(string="Cantidad por vencer")
-    total = fields.Float(string="Total Deuda")
 
     #@api.model
     #def create(self,vals):
