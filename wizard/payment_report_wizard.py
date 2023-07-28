74ca50f3d3f0661b8a9e59e421a3ec220be27f81
mgah77 <mauricio.gah@gmail.com>
2023-07-27 22:44:53 -0400

    Update payment_report_wizard.py

--- wizard/payment_report_wizard.py
+++ wizard/payment_report_wizard.py
@@ -1,10 +1,12 @@
 
 from odoo import models, fields, api
 
+from datetime import date
+
 class PaymentWizard(models.Model):
     _name = "payment.wizard"
 
-    date = fields.Date(default=fields.Date.today, required=True)
+    today = date.today()
     cliente = fields.Many2one('res.partner', string='Cliente', domain=[('is_company', '=', True)])
     fac_vencido = fields.Integer(string="FacturasVencidas", compute='_compute_cantidad_vencida')
     vencido = fields.Float(string="Cantidad Vencida", compute='_compute_cantidad_vencida')
@@ -21,7 +23,6 @@ class PaymentWizard(models.Model):
         #data['form'] = v
         data['ids'] = self.env.context.get('active_ids', [])
         data['model'] = self.env.context.get('active_model', 'payment.wizard')
-        data['date'] = self.date
         data['cliente'] = self.cliente
         data['fac_vencido'] = self.cliente
         return self.env.ref('payment_report.action_payment_report').report_action(self, data=data)
