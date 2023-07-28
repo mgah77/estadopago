1547d5d07d60383f6edbc0cfb5c8c677e7b79459
mgah77 <mauricio.gah@gmail.com>
2023-07-27 20:39:39 -0400

    Update payment_report_wizard.py

--- wizard/payment_report_wizard.py
+++ wizard/payment_report_wizard.py
@@ -26,15 +26,14 @@ class PaymentWizard(models.Model):
         data['model'] = self.env.context.get('active_model', 'payment.wizard')
         data['date'] = self.date
         data['cliente'] = self.cliente
-
         return self.env.ref('payment_report.action_payment_report').report_action(self, data=data)
 
     @api.depends('cliente')
     def _compute_cantidad_vencida(self):
-        Invoice = self.env['account.invoice']
-        for record in self:
-            if record.cliente:
-                fac_vencido = Invoice.search_count([('partner_id', '=', record.cliente.id)])
-                record.fac_vencido = fac_vencido
-            else:
-                record.fac_vencido = 0
+    #    Invoice = self.env['account.invoice']
+    #    for record in self:
+    #        if record.cliente:
+    #            fac_vencido = Invoice.search_count([('partner_id', '=', record.cliente.id)])
+    #            record.fac_vencido = fac_vencido
+    #        else:
+    #            record.fac_vencido = 0
