b678ea1a2926336a1dabb6149a483741a14be141
mgah77 <mauricio.gah@gmail.com>
2023-07-27 22:52:48 -0400

    Update payment_report_wizard.py

--- wizard/payment_report_wizard.py
+++ wizard/payment_report_wizard.py
@@ -9,6 +9,8 @@ class PaymentWizard(models.Model):
     cliente = fields.Many2one('res.partner', string='Cliente', domain=[('is_company', '=', True)])
     fac_vencido = fields.Integer(string="FacturasVencidas", compute='_compute_cantidad_vencida')
     vencido = fields.Float(string="Cantidad Vencida", compute='_compute_cantidad_vencida')
+    pre_fac_vencido = fields.Integer(string="Facturas por vencer", compute='_compute_cantidad_vencida')
+    pre_vencido = fields.Float(string="Cantidad por vencer", compute='_compute_cantidad_vencida')
 
     def name_get(self):
         result = []
@@ -36,7 +38,14 @@ class PaymentWizard(models.Model):
                 vencido = Invoice.search([('partner_id', '=', record.cliente.id),('type', '=', 'out_invoice'),('state', '=', 'open'),('date_due', '<=', fields.Date.today())])
                 total_vencido = sum(factura.amount_total for factura in vencido)
                 record.vencido = total_vencido
+                pre_fac_vencido = Invoice.search_count([('partner_id', '=', record.cliente.id),('type', '=', 'out_invoice'),('state', '=', 'open'),('date_due', '>', fields.Date.today())])
+                record.pre_fac_vencido = pre_fac_vencido
+                pre_vencido = Invoice.search([('partner_id', '=', record.cliente.id),('type', '=', 'out_invoice'),('state', '=', 'open'),('date_due', '>', fields.Date.today())])
+                total_pre_vencido = sum(factura.amount_total for factura in pre_vencido)
+                record.pre_vencido = total_pre_vencido
             else:
                 record.fac_vencido = 0
                 record.vencido = 0
+                record.pre_fac_vencido = 0
+                record.pre_vencido = 0
         return
