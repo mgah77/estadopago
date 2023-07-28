50a02d7a5d4dd12475a0ae8dc5a2537c114b076e
mgah77 <mauricio.gah@gmail.com>
2023-07-27 20:04:54 -0400

    Update payment_report_wizard.py

--- wizard/payment_report_wizard.py
+++ wizard/payment_report_wizard.py
@@ -7,7 +7,7 @@ class PaymentWizard(models.Model):
     _name = "payment.wizard"
 
     date = fields.Date(default=fields.Date.today, required=True)
-
+    cliente = fields.Many2one('res.partner', string='Cliente')
 
     def action_print_report(self):
         data = {}
@@ -15,5 +15,6 @@ class PaymentWizard(models.Model):
         data['ids'] = self.env.context.get('active_ids', [])
         data['model'] = self.env.context.get('active_model', 'payment.wizard')
         data['date'] = self.date
+        data['cliente'] = self.cliente
 
         return self.env.ref('payment_report.action_payment_report').report_action(self, data=data)
